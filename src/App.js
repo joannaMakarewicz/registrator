5fa619d91cd2c538031f1b69e852ec521f837268
Joanna Makarewicz <joanna.milun@gmail.com>
2023-01-31 12:22:47 +0100

    props as array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import './App.css';
-import Header from './components/Header';
-import User from './components/User';
+import "./App.css";
+import Header from "./components/Header";
+import User from "./components/User";
 
 function App() {
+  const users = [
+    {
+      username: "Julia",
+      surname: "Roberts",
+      age: 55,
+    },
+    {
+      username: "Leonardo",
+      surname: "DiCaprio",
+      age: 48,
+    },
+    {
+      username: "Ben",
+      surname: "Affleck",
+      age: 50,
+    },
+    {
+      username: "Hugh",
+      surname: "Grant",
+      age: 62,
+    }
+  ];
 
-  const newUser = {
-    username: 'Joanna',
-    surname: 'Makarewicz',
-    age: 37
-  }
   return (
     <div>
-      <Header/>
-      <User username={newUser.username} surname={newUser.surname} age={newUser.age} />
+      <Header />
+      <User
+       users={users}
+      />
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const User = ({username, surname, age}) => {
+const User = ({ users }) => {
   return (
     <div>
-      <h2>New user</h2>
-      <ul>
-        <li>Username: {username}</li>
-        <li>Surname: {surname}</li>
-        <li>Age: {age}</li>
-      </ul>
+      <h2>Our users</h2>
+      {users.map((user, index) => {
+        return (
+          <ul key="index">
+            <li>Username: {user.username}</li>
+            <li>Surname: {user.surname}</li>
+            <li>Age: {user.age}</li>
+          </ul>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default User;
