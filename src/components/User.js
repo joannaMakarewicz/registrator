d12183d193558d50da34336873a1e31ece0c2b3a
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-02 11:53:32 +0100

    bug delated

--- src/components/User.js
+++ src/components/User.js
@@ -7,7 +7,7 @@ const User = ({ users }) => {
       <h2>Our users</h2>
       {users.map((user, index) => {
         return (
-          <ul key="index">
+          <ul key={index}>
             <li>Username: {user.username} </li>
             <li>Surname: {user.surname}</li>
             <li>Age: {user.age}</li>
