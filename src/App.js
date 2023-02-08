8257d9295987672ac48965cb375541b7d6a4de10
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-08 12:14:13 +0100

    Navigate added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 
 
 function App() {
   return (
+    <div>
+      <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path="/" element={<Navigate to='registrator'/>}/>
+          <Route path='/registrator' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
+      </Router>
+    </div>
   );
 }
 
