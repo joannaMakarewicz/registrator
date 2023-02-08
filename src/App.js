7476bf1c86b8e98a40eaede494c8a2b075d18b66
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-08 11:30:28 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 
+
 function App() {
   return (
-    <div>
-      <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
-      </Router>
-    </div>
   );
 }
 
